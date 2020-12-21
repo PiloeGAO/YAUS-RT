81159b776a037ad91793b4b66617c094c0bed8ff
PiloeGAO <leonumerique@gmail.com>
2020-12-21 10:52:19 +0100

    ADD: Header to main program.

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,6 @@
+#include "vec3.h"
+#include "color.h"
+
 #include <iostream>
 
 int main()
@@ -18,17 +21,8 @@ int main()
         std::cerr << "\rScanlines remaining: " << j << " " << std::flush;
         for (int i = 0; i < image_width; i++)
         {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
-
-            // Set colors as 8bits [0 - 255].
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            // Print result in the console.
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
 
